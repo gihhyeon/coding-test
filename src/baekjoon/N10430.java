c7341cb966b4c9e6d67d39869183a4ff656d038a
gihyeon <fineblue.kh@gmail.com>
2025-01-15 17:33:32 +0900

    백준 10430q번

---
+++ src/baekjoon/N10430.java
@@ -0,0 +1,19 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class N10430 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A, B, C;
+        A = scanner.nextInt();
+        B = scanner.nextInt();
+        C = scanner.nextInt();
+
+        System.out.println((A+B)%C);
+        System.out.println(((A%C) + (B%C))%C);
+        System.out.println((A*B)%C);
+        System.out.println(((A%C) * (B%C))%C);
+
+    }
+}
