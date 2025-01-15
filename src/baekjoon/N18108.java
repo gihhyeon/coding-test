a47ec1dc2b3bd03a72b160d83a362acbb4ddec3b
gihyeon <fineblue.kh@gmail.com>
2025-01-15 17:17:53 +0900

    백준 18108번

---
+++ src/baekjoon/N18108.java
@@ -0,0 +1,12 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class N18108 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int y;
+        y = scanner.nextInt();
+        System.out.println(y - 543);
+    }
+}
