9ad2e25901c7ef0e8288a4281550506337e8edbc
gihyeon <fineblue.kh@gmail.com>
2025-01-15 16:58:55 +0900

    백준 1008번

---
+++ src/baekjoon/N1008.java
@@ -0,0 +1,16 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class N1008 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A, B;
+        A = scanner.nextInt();
+        B = scanner.nextInt();
+        System.out.println((double)A / B);
+    }
+
+    // 형 변환을 생각합시다.
+}
