a61519af676d6c987c4c963b757b4e235de40f1e
gihyeon <fineblue.kh@gmail.com>
2025-01-15 17:05:58 +0900

    N10869

---
+++ src/baekjoon/N10869.java
@@ -0,0 +1,18 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class N10869 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int A, B;
+        A = scanner.nextInt();
+        B = scanner.nextInt();
+        System.out.println(A+B);
+        System.out.println(A-B);
+        System.out.println(A*B);
+        System.out.println(A/B);
+        System.out.println(A%B);
+
+    }
+}
