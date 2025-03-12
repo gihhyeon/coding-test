3ac3430c122cb0f08bb75158cff40370fc58bda7
gihhyeon <fineblue.kh@gmail.com>
2025-03-12 23:11:03 +0900

    백준 2738번

---
+++ src/baekjoon/level7/N2738.java
@@ -0,0 +1,41 @@
+package baekjoon.level7;
+
+import java.util.Scanner;
+
+public class N2738 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        int[][] arr = new int[N][M];
+        int[][] arr2 = new int[N][M];
+        int[][] sum = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                arr2[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                sum[i][j] = arr[i][j] + arr2[i][j];
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                System.out.print(sum[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
