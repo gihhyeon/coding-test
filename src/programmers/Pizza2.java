5932e81d6478c686d02de05a069771c76f75706b
gihhyeon <fineblue.kh@gmail.com>
2025-06-25 22:14:54 +0900

    피자 나눠먹기2

---
+++ src/programmers/Pizza2.java
@@ -0,0 +1,16 @@
+package programmers;
+
+public class Pizza2 {
+    public int solution(int n) {
+        int answer = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (i * 6 % n == 0) {
+                answer = i;
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
