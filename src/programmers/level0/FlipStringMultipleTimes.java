df67552924f2e7bcb4a3ba6c7599e519859a03f8
gihhyeon <fineblue.kh@gmail.com>
2025-07-14 21:16:08 +0900

    문자열 여러 번 뒤집기

---
+++ src/programmers/level0/FlipStringMultipleTimes.java
@@ -0,0 +1,20 @@
+package programmers.level0;
+
+public class FlipStringMultipleTimes {
+
+    public StringBuilder solution(String my_string, int[][] queries) {
+        StringBuilder str = new StringBuilder(my_string);
+
+        for (int i = 0; i < queries.length; i++) {
+            int start = queries[i][0];
+            int end = queries[i][1];
+
+            StringBuilder s = new StringBuilder(str.substring(start, end+1));
+            s.reverse();
+            str.replace(start, end + 1, s.toString());
+        }
+
+        return str;
+    }
+
+}
