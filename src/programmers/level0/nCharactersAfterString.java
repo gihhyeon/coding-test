cb7611944194fe1328cfce34a1fe890f158cc1da
gihhyeon <fineblue.kh@gmail.com>
2025-07-16 09:38:30 +0900

    문자열 뒤의 n 글자

---
+++ src/programmers/level0/nCharactersAfterString.java
@@ -0,0 +1,12 @@
+package programmers.level0;
+
+public class nCharactersAfterString {
+
+    public String solution(String my_string, int n) {
+        String answer = "";
+
+        answer = my_string.substring(my_string.length() - n);
+
+        return answer;
+    }
+}
