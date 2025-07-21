c8a024da35c4db5156990c43c212c383b373ac89
gihhyeon <fineblue.kh@gmail.com>
2025-07-21 21:18:45 +0900

    간단한 논리 연산

---
+++ src/programmers/level0/SimpleLogicalOperations.java
@@ -0,0 +1,14 @@
+package programmers.level0;
+
+public class SimpleLogicalOperations {
+
+    public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
+
+        if ((x1 || x2) && (x3 || x4)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
