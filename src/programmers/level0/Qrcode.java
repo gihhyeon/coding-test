9028eab979c5e32707b6515773c63b725d6e28a1
gihhyeon <fineblue.kh@gmail.com>
2025-07-22 21:18:04 +0900

    qr code

---
+++ src/programmers/level0/Qrcode.java
@@ -0,0 +1,18 @@
+package programmers.level0;
+
+public class Qrcode {
+
+    public String solution(int q, int r, String code) {
+        StringBuilder answer = new StringBuilder();
+
+
+        for (int i = 0; i < code.length(); i++) {
+            if (i % q == r) {
+                answer.append(code.charAt(i));
+            }
+        }
+
+        return answer.toString();
+    }
+
+}
