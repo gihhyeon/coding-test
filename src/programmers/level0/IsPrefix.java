d1513ec4c906885c8e905ae8cb2dd85efca7d8ad
gihhyeon <fineblue.kh@gmail.com>
2025-07-22 20:59:46 +0900

    접두산이지 확인하기

---
+++ src/programmers/level0/IsPrefix.java
@@ -0,0 +1,15 @@
+package programmers.level0;
+
+public class IsPrefix {
+
+    public int solution(String my_string, String is_prefix) {
+        int answer = 0;
+
+        if (my_string.startsWith(is_prefix)) {
+            answer = 1;
+        }
+
+        return answer;
+    }
+
+}
