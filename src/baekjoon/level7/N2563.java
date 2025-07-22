fd282ad582cadfe857518e141e44a3f32faa47ad
gihhyeon <fineblue.kh@gmail.com>
2025-07-22 21:53:15 +0900

    백준 2563번

---
+++ src/baekjoon/level7/N2563.java
@@ -0,0 +1,28 @@
+package baekjoon.level7;
+
+import java.util.Scanner;
+
+public class N2563 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean[][] paper = new boolean[100][100];
+        int n = sc.nextInt();
+        int area = 0;
+
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            for (int dx = x; dx < x + 10; dx++) {
+                for (int dy = y; dy < y + 10; dy++) {
+                    if (!paper[dx][dy]) {
+                        paper[dx][dy] = true;
+                        area++;
+                    }
+                }
+            }
+        }
+
+        System.out.println(area);
+    }
+}
