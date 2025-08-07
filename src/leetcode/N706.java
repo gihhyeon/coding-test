40b2fabdcb95e4f4e52b2cb732ec7f2e5286980b
gihhyeon <fineblue.kh@gmail.com>
2025-08-07 22:37:09 +0900

    leetcode: Design HashMap

---
+++ src/leetcode/N706.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+
+import java.util.Arrays;
+
+// Design HashMap
+public class N706 {
+    private int[] map;
+
+    public N706() {
+        map = new int[1000001];
+        Arrays.fill(map, -1);
+    }
+
+    public void put(int key, int value) {
+        map[key] = value;
+    }
+
+    public int get(int key) {
+        return map[key];
+    }
+
+    public void remove(int key) {
+        map[key] = -1;
+    }
+}
