acc66ca040518a54377c44efc3737fd716cc7fff
gihhyeon <fineblue.kh@gmail.com>
2025-08-11 20:03:05 +0900

    leetcode: 70 Climbing Stairs

---
+++ src/leetcode/N70.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+// Climbing Stairs
+public class N70 {
+    public int climbStairs(int n) {
+        if (n <= 2) {
+            return n;
+        }
+
+        int[] arr = new int[n + 1];
+        arr[1] = 1;
+        arr[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+
+        return arr[n];
+    }
+}
