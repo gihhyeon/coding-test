6ff5bf69e801df41fadd7d1d76216e1cdcf8e1d2
gihhyeon <fineblue.kh@gmail.com>
2025-08-14 16:47:38 +0900

    leetcode: 349번

---
+++ src/leetcode/N349.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+//  Intersection of Two Arrays
+import java.util.*;
+
+public class N349 {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int num : nums1) {
+            set.add(num);
+        }
+
+        Set<Integer> resultSet = new HashSet<>();
+
+        for (int num : nums2) {
+            if (set.contains(num)) {
+                resultSet.add(num);
+            }
+        }
+
+        int[] result = new int[resultSet.size()];
+        int i = 0;
+        for (int num : resultSet) {
+            result[i++] = num;
+        }
+
+        return result;
+    }
+}
