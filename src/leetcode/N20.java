174eed4c32d83053aa53320d3c52aa43e3ad5bba
gihhyeon <fineblue.kh@gmail.com>
2025-09-25 14:45:48 +0900

    leetcode: 20. Valid Parenthese

---
+++ src/leetcode/N20.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+import java.util.Stack;
+
+public class N20 {
+
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.peek();
+
+                if ((c == ')' && top != '(')
+                        || (c == '}' && top != '{')
+                        || (c == ']' && top != '[')) {
+                    return false;
+                }
+
+                stack.pop();
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
