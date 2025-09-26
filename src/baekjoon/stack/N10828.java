b76e3537edbdf417bf1e8dd6dc34b00158d4bb58
gihhyeon <fineblue.kh@gmail.com>
2025-09-26 13:46:21 +0900

    백준: 10828번

---
+++ src/baekjoon/stack/N10828.java
@@ -0,0 +1,81 @@
+package baekjoon.stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class N10828 {
+
+    static int[] stack;
+    static int size = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        stack = new int[N];
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String cmd = st.nextToken();
+
+            switch (cmd) {
+                case "push":
+                    push(Integer.parseInt(st.nextToken()));
+                    break;
+                case "pop":
+                    sb.append(pop()).append('\n');
+                    break;
+                case "size":
+                    sb.append(size()).append('\n');
+                    break;
+                case "empty":
+                    sb.append(empty()).append('\n');
+                    break;
+                case "top":
+                    sb.append(top()).append('\n');
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        System.out.print(sb);
+    }
+
+    public static void push(int num) {
+        stack[size] = num;
+        size++;
+    }
+
+    public static int pop() {
+        if (size == 0) {
+            return -1;
+        } else {
+            size--;
+            return stack[size];
+        }
+    }
+
+    public static int size() {
+        return size;
+    }
+
+    public static int empty() {
+        if (size == 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    public static int top() {
+        if (size == 0) {
+            return -1;
+        } else {
+            return stack[size - 1];
+        }
+    }
+}
