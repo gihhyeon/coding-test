95efc53239e5072a82bad5354790af92ce8a3580
gihhyeon <fineblue.kh@gmail.com>
2025-10-02 12:27:30 +0900

    leetcode: 121번

---
+++ src/leetcode/N121.java
@@ -0,0 +1,17 @@
+package leetcode;
+
+public class N121 {
+
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+        int minPrice = Integer.MAX_VALUE;
+
+        for (int price : prices) {
+            minPrice = Math.min(minPrice, price);
+            maxProfit = Math.max(maxProfit, price - minPrice);
+        }
+
+        return maxProfit;
+
+    }
+}
