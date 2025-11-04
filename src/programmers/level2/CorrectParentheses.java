c619a3b0c0d4d4cb61a924069a5f2cceb6507f45
gihhyeon <fineblue.kh@gmail.com>
2025-11-04 11:24:21 +0900

    프로그래머스: 올바른 괄호

---
+++ src/programmers/level2/CorrectParentheses.java
@@ -0,0 +1,21 @@
+package programmers.level2;
+
+import java.util.Stack;
+
+public class CorrectParentheses {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+}
