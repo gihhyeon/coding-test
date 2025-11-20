aa0f1834d9da19a33a978a6b81ab3e080139ef87
gihhyeon <fineblue.kh@gmail.com>
2025-11-20 21:33:42 +0900

    leetcode: Longest Common Prefix

---
+++ src/leetcode/N14.java
@@ -0,0 +1,20 @@
+package leetcode;
+
+public class N14 {
+    public String longestCommonPrefix(String[] strs) {
+
+        String prefix = strs[0];
+
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+
+            for (int j = 1; j < strs.length; j++) {
+                if (i == strs[j].length() || strs[j].charAt(i) != c) {
+                    return prefix.substring(0, i);
+                }
+            }
+        }
+
+        return prefix;
+    }
+}
