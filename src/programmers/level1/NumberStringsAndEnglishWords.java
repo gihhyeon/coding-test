c871eaa269254b48a284615966e6062797662a05
gihhyeon <fineblue.kh@gmail.com>
2025-12-24 23:34:55 +0900

    프로그래머스: 숫자 문자열과 영단어

--- src/programmers/level1/NumberStringsAndEnglishWords.java
+++ src/programmers/level1/NumberStringsAndEnglishWords.java
@@ -1,12 +1,22 @@
 package programmers.level1;
 
 public class NumberStringsAndEnglishWords {
+//    public int solution(String s) {
+//        String[] arr = {"zero", "one", "two", "three", "four", "five",
+//                "six", "seven", "eight", "nine"};
+//
+//        for (int i = 0; i < arr.length; i++) {
+//            s = s.replace(arr[i], Integer.toString(i));
+//        }
+//
+//        return Integer.parseInt(s);
+//    }
     public int solution(String s) {
-        String[] arr = {"zero", "one", "two", "three", "four", "five",
-                "six", "seven", "eight", "nine"};
+        String[] arr = {"zero", "one", "two", "three", "four",
+                "five", "six", "seven", "eight", "nine"};
 
         for (int i = 0; i < arr.length; i++) {
-            s = s.replace(arr[i], Integer.toString(i));
+            s = s.replace(arr[i], i + "");
         }
 
         return Integer.parseInt(s);
