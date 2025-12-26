d393589d4bc2c6fe34cb7cd9cbd6775c5bc77b13
gihhyeon <fineblue.kh@gmail.com>
2025-12-26 16:43:10 +0900

    leetcode: 217.Contains Duplicate

---
+++ src/leetcode/N217.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class N217 {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (set.add(nums[i]) == false) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
