a7481e9a30b104b7ab941d987cf2ae3ed64a7974
gihhyeon <fineblue.kh@gmail.com>
2025-12-29 22:13:45 +0900

    프로그래머스: 폰켓몬

--- src/programmers/level1/Ponkemon.java
+++ src/programmers/level1/Ponkemon.java
@@ -1,21 +1,40 @@
 package programmers.level1;
 
 import java.util.HashSet;
+import java.util.Set;
 
 public class Ponkemon {
+//    public int solution(int[] nums) {
+//        int answer = 0;
+//
+//        HashSet<Integer> set = new HashSet<>();
+//
+//        for (int n : nums) {
+//            set.add(n);
+//        }
+//
+//        int length = nums.length / 2;
+//
+//        if (set.size() >= length) {
+//            answer = length;
+//        } else {
+//            answer = set.size();
+//        }
+//
+//        return answer;
+//    }
+
     public int solution(int[] nums) {
         int answer = 0;
 
-        HashSet<Integer> set = new HashSet<>();
+        Set<Integer> set = new HashSet<>();
 
-        for (int n : nums) {
-            set.add(n);
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
         }
 
-        int length = nums.length / 2;
-
-        if (set.size() >= length) {
-            answer = length;
+        if (set.size() >= nums.length / 2) {
+            answer = nums.length / 2;
         } else {
             answer = set.size();
         }
