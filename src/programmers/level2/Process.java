6ba88b68d502d93eb6182cddc1bed49b9e4eb03d
gihhyeon <fineblue.kh@gmail.com>
2026-01-05 22:14:19 +0900

    프로그래머스: 프로세스

--- src/programmers/level2/Process.java
+++ src/programmers/level2/Process.java
@@ -1,27 +1,62 @@
 package programmers.level2;
 
-import java.util.Collections;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class Process {
+//    class Solution {
+//        public int solution(int[] priorities, int location) {
+//            PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+//            int answer = 0;
+//
+//            for (int n : priorities) {
+//                pq.add(n);
+//            }
+//
+//            while (!pq.isEmpty()) {
+//                for (int i = 0; i < pq.size(); i++) {
+//                    if (pq.peek() == priorities[i]) {
+//                        pq.poll();
+//                        answer++;
+//
+//                        if (location == i) {
+//                            return answer;
+//                        }
+//                    }
+//                }
+//            }
+//            return answer;
+//        }
+//    }
+
     class Solution {
         public int solution(int[] priorities, int location) {
-            PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
             int answer = 0;
+            Queue<Integer> queue = new LinkedList<>();
 
-            for (int n : priorities) {
-                pq.add(n);
+            for (int p : priorities) {
+                queue.add(p);
             }
 
-            while (!pq.isEmpty()) {
-                for (int i = 0; i < pq.size(); i++) {
-                    if (pq.peek() == priorities[i]) {
-                        pq.poll();
-                        answer++;
+            Arrays.sort(priorities);
+            int index = priorities.length - 1;
 
-                        if (location == i) {
-                            return answer;
-                        }
+            while (!queue.isEmpty()) {
+                int current = queue.poll();
+
+                if (current == priorities[index]) {
+                    answer++;
+                    if (location == 0) {
+                        return answer;
+                    }
+
+                    index--;
+                    location--;
+                } else {
+                    queue.add(current);
+                    if (location == 0) {
+                        location = queue.size() - 1;
+                    } else {
+                        location--;
                     }
                 }
             }
