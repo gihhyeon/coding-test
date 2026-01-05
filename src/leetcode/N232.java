734b32336b2690993a5b3b32ca6fc0b612fcf123
gihhyeon <fineblue.kh@gmail.com>
2026-01-05 18:15:03 +0900

    leetcode: 232.Implement Queue using Stacks

---
+++ src/leetcode/N232.java
@@ -0,0 +1,40 @@
+package leetcode;
+
+import java.util.Stack;
+
+public class N232 {
+    Stack<Integer> stack1 = new Stack<>();
+    Stack<Integer> stack2 = new Stack<>();
+
+    public N232() {
+
+    }
+
+    public void push(int x) {
+        stack1.push(x);
+    }
+
+    public int pop() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+
+        return stack2.pop();
+    }
+
+    public int peek() {
+        if (stack2.isEmpty()) {
+            while (!stack1.isEmpty()) {
+                stack2.push(stack1.pop());
+            }
+        }
+
+        return stack2.peek();
+    }
+
+    public boolean empty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+}
