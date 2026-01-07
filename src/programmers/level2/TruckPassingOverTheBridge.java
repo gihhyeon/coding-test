36fbea8dadea4e7ba0624978c2b75196af49497c
gihhyeon <fineblue.kh@gmail.com>
2026-01-07 20:19:56 +0900

    프로그래머스: 다리를 지나는 트럭

---
+++ src/programmers/level2/TruckPassingOverTheBridge.java
@@ -0,0 +1,32 @@
+package programmers.level2;
+
+import java.util.*;
+
+public class TruckPassingOverTheBridge {
+    public int solution(int bridge_length, int weight, int[] truck_weights) {
+        int time = 0;
+        int currentWeight = 0;
+        int index = 0;
+        Queue<Integer> bridge = new LinkedList<>();
+
+        for (int i = 0; i < bridge_length; i++) {
+            bridge.add(0);
+        }
+
+        while (!bridge.isEmpty()) {
+            time++;
+            currentWeight -= bridge.poll();
+
+            if (index < truck_weights.length) {
+                if (currentWeight + truck_weights[index] <= weight) {
+                    bridge.add(truck_weights[index]);
+                    currentWeight += truck_weights[index];
+                    index++;
+                } else {
+                    bridge.add(0);
+                }
+            }
+        }
+        return time;
+    }
+}
