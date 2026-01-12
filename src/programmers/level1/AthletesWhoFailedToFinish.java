8f2db7f643dced6c64e3d6c074a20f987258729d
gihhyeon <fineblue.kh@gmail.com>
2026-01-12 20:44:35 +0900

    프로그래머스: 완주하지 못한 선수

--- src/programmers/level1/AthletesWhoFailedToFinish.java
+++ src/programmers/level1/AthletesWhoFailedToFinish.java
@@ -4,23 +4,44 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class AthletesWhoFailedToFinish {
+//    public String solution(String[] participant, String[] completion) {
+//        Map<String, Integer> map = new HashMap<>();
+//
+//        for (String p : participant) {
+//            map.put(p, map.getOrDefault(p, 0) + 1);
+//        }
+//
+//        for (String c : completion) {
+//            map.put(c, map.get(c) - 1);
+//        }
+//
+//        for (String name : map.keySet()) {
+//            if (map.get(name) > 0) {
+//                return name;
+//            }
+//        }
+//
+//        return "";
+//    }
+
     public String solution(String[] participant, String[] completion) {
+        String answer = "";
         Map<String, Integer> map = new HashMap<>();
 
-        for (String p : participant) {
-            map.put(p, map.getOrDefault(p, 0) + 1);
+        for (String player : participant) {
+            map.put(player, map.getOrDefault(player, 0) + 1);
         }
 
-        for (String c : completion) {
-            map.put(c, map.get(c) - 1);
+        for (String player : completion) {
+            map.put(player, map.get(player) - 1);
         }
 
-        for (String name : map.keySet()) {
-            if (map.get(name) > 0) {
-                return name;
+        for (String key : map.keySet()) {
+            if (map.get(key) > 0) {
+                answer = key;
+                break;
             }
         }
-
-        return "";
+        return answer;
     }
 }
