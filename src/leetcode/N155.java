719010639c4a848db2d44e75df6281f1367519f0
gihhyeon <fineblue.kh@gmail.com>
2026-01-12 15:54:01 +0900

    leetcode: 155. Min Stack

---
+++ src/leetcode/N155.java
@@ -0,0 +1,37 @@
+package leetcode;
+
+import java.util.Stack;
+
+public class N155 {
+
+    Stack<Integer> stack;
+    Stack<Integer> minStack;
+
+    public N155() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+
+        if (minStack.isEmpty()) {
+            minStack.push(val);
+        } else {
+            minStack.push(Math.min(val, minStack.peek()));
+        }
+    }
+
+    public void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
