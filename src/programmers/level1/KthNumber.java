f0b3605d8ec296d9274b1f1afe31508c96ed8a78
gihhyeon <fineblue.kh@gmail.com>
2026-01-15 14:37:05 +0900

    프로그래머스: K번째수

--- src/programmers/level1/KthNumber.java
+++ src/programmers/level1/KthNumber.java
@@ -4,19 +4,31 @@ import java.util.Arrays;
 
 public class KthNumber {
 
+//    public int[] solution(int[] array, int[][] commands) {
+//        int[] answer = new int[commands.length];
+//
+//        for (int i = 0; i < commands.length; i++) {
+//            int start = commands[i][0] - 1;
+//            int end = commands[i][1] - 1;
+//            int k = commands[i][2] - 1;
+//
+//            int[] temp = Arrays.copyOfRange(array, start, end + 1);
+//            Arrays.sort(temp);
+//            answer[i] = temp[k];
+//        }
+//
+//        return answer;
+//    }
+
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
+        int[] sliced = new int[commands.length];
 
         for (int i = 0; i < commands.length; i++) {
-            int start = commands[i][0] - 1;
-            int end = commands[i][1] - 1;
-            int k = commands[i][2] - 1;
-
-            int[] temp = Arrays.copyOfRange(array, start, end + 1);
-            Arrays.sort(temp);
-            answer[i] = temp[k];
+            sliced = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(sliced);
+            answer[i] = sliced[commands[i][2] - 1];
         }
-
         return answer;
     }
 }
