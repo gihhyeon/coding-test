68759e798843348bb14206b9ec76677d2d2c7a71
gihhyeon <fineblue.kh@gmail.com>
2026-01-19 21:42:16 +0900

    프로그래머스: 전화번호 목록

--- src/programmers/level2/PhoneNumberList.java
+++ src/programmers/level2/PhoneNumberList.java
@@ -3,15 +3,27 @@ package programmers.level2;
 import java.util.Arrays;
 
 public class PhoneNumberList {
-    public boolean solution(String[] phone_book) {
+//    public boolean solution(String[] phone_book) {
+//
+//        Arrays.sort(phone_book);
+//
+//        for (int i = 0; i < phone_book.length - 1; i++) {
+//            String current = phone_book[i];
+//            String next = phone_book[i + 1];
+//
+//            if (next.startsWith(current)) {
+//                return false;
+//            }
+//        }
+//
+//        return true;
+//    }
 
+    public boolean solution(String[] phone_book) {
         Arrays.sort(phone_book);
 
-        for (int i = 0; i < phone_book.length - 1; i++) {
-            String current = phone_book[i];
-            String next = phone_book[i + 1];
-
-            if (next.startsWith(current)) {
+        for (int i = 1; i < phone_book.length; i++) {
+            if (phone_book[i].startsWith(phone_book[i-1])) {
                 return false;
             }
         }
