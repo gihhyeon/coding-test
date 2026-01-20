27b5eebf0e525c77873fd4178d0f61634a1aff2e
gihhyeon <fineblue.kh@gmail.com>
2026-01-20 21:39:47 +0900

    프로그래머스: 가장 큰 수

---
+++ src/programmers/level2/BiggestNumber.java
@@ -0,0 +1,26 @@
+package programmers.level2;
+
+import java.util.Arrays;
+
+public class BiggestNumber {
+    public String solution(int[] numbers) {
+        String[] strNums = new String[numbers.length];
+
+        for (int i = 0; i < numbers.length; i++) {
+            strNums[i] = String.valueOf(numbers[i]);
+        }
+
+        Arrays.sort(strNums, (a, b) -> (b + a).compareTo(a + b));
+
+        if (strNums[0].equals("0")) {
+            return "0";
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (String s : strNums) {
+            sb.append(s);
+        }
+
+        return sb.toString();
+    }
+}
