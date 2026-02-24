28ef34440c645e6dac3e3132816d80383cbc569b
gihhyeon <fineblue.kh@gmail.com>
2026-02-24 20:25:44 +0900

    백준: 크로아티아 알파벳

---
+++ src/baekjoon/N2941.java
@@ -0,0 +1,22 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class N2941 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        String[] croatia = {"c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="};
+
+        for (String pattern : croatia) {
+            str = str.replace(pattern, "*");
+        }
+
+        System.out.println(str.length());
+
+    }
+}
