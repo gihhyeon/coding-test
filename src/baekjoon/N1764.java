bf63a1109a2a8854b4e10665824e4f57a69952a0
gihhyeon <fineblue.kh@gmail.com>
2026-02-26 22:30:06 +0900

    백준: 듣보잡

---
+++ src/baekjoon/N1764.java
@@ -0,0 +1,40 @@
+package baekjoon;
+
+import java.util.*;
+import java.io.*;
+
+public class N1764 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        HashSet<String> unheard = new HashSet<>();
+        for (int i = 0; i < N; i++) {
+            unheard.add(br.readLine());
+        }
+
+        ArrayList<String> result = new ArrayList<>();
+        for (int i = 0; i < M; i++) {
+            String name = br.readLine();
+            if (unheard.contains(name)) {
+                result.add(name);
+            }
+        }
+
+        Collections.sort(result);
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(result.size()).append("\n");
+
+        for (String name : result) {
+            sb.append(name).append("\n");
+        }
+
+        System.out.println(sb.toString());
+
+
+    }
+}
