2f301b708ddeaa03e0f1309fabffc3a9c598adbe
gihhyeon <fineblue.kh@gmail.com>
2026-02-26 22:29:21 +0900

    프로그래머스: 같은 숫자는 싫어

--- src/programmers/level1/HateSameNumbers.java
+++ src/programmers/level1/HateSameNumbers.java
@@ -3,22 +3,43 @@ package programmers.level1;
 import java.util.Stack;
 
 public class HateSameNumbers {
-    public Stack solution(int []arr) {
+//    public Stack solution(int []arr) {
+//
+//        Stack<Integer> stack = new Stack<Integer>();
+//
+//        for (int i = 0; i < arr.length; i++) {
+//            if (stack.isEmpty() || stack.peek() != arr[i]) {
+//                stack.push(arr[i]);
+//            }
+//        }
+//
+////         int[] answer = new int[stack.size()];
+//
+////         for (int i = stack.size() - 1; i >= 0; i--) {
+////             answer[i] = stack.pop();
+////         }
+//
+//        return stack;
+//    }
 
-        Stack<Integer> stack = new Stack<Integer>();
+public int[] solution(int[] arr) {
 
-        for (int i = 0; i < arr.length; i++) {
-            if (stack.isEmpty() || stack.peek() != arr[i]) {
-                stack.push(arr[i]);
-            }
+    Stack<Integer> stack = new Stack<>();
+
+    stack.push(arr[0]);
+
+    for (int i = 1; i < arr.length; i++) {
+        if (stack.peek() != arr[i]) {
+            stack.push(arr[i]);
         }
-
-//         int[] answer = new int[stack.size()];
-
-//         for (int i = stack.size() - 1; i >= 0; i--) {
-//             answer[i] = stack.pop();
-//         }
-
-        return stack;
     }
+
+    int[] answer = new int[stack.size()];
+
+    for (int i = 0; i < stack.size(); i++) {
+        answer[i] = stack.get(i);
+    }
+
+    return answer;
+}
 }
