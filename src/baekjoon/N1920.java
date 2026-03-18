686eb9823e8f238e21a7de16fe7cfa728286d87b
gihhyeon <fineblue.kh@gmail.com>
2026-03-18 23:12:13 +0900

    백준: 수 찾기

---
+++ src/baekjoon/N1920.java
@@ -0,0 +1,33 @@
+package baekjoon;
+
+import java.util.*;
+import java.io.*;
+
+public class N1920 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        HashSet<Integer> set = new HashSet<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            set.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            if (set.contains(Integer.parseInt(st.nextToken()))) {
+                sb.append("1").append("\n");
+            } else {
+                sb.append("0").append("\n");
+            }
+        }
+
+        System.out.print(sb.toString());
+    }
+}
